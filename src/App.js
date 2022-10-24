0a48c03e04455cadd66ba3970a6ec13e9e9e74c1
Sam <samuel.waters@gmail.com>
2022-10-24 16:38:05 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+
+
+ 
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/"> 
+            <Home />
+          </Route>
+          <Route path="/About"> 
+            <About /> 
+          </Route>
+          <Route path="/Contact">
+            <Contact /> 
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+
+
+{/* <Switch>
+  <Route exact path="/">
+    {showCurrentBook ?
+      <BookDisplay book={currentBook} /> :
+      <Home onHandleDisplay={HandleDisplay} userAuthorized={userAuthorized} />}
+  </Route>
+  <Route exact path="/admin">
+    <Admin books={displayedBooks} setBooks={setBooks} adminAuthorized={adminAuthorized} />
+  </Route>
+  <Route exact path="/books">
+    {showCurrentBook ?
+      <BookDisplay book={currentBook} /> :
+      <Books userAuthorized={userAuthorized} onHandleDisplay={HandleDisplay} />}
+  </Route>
+
+  <Route exact path="/login">
+    <Login userAuthorized={userAuthorized} setUserAuthorized={setUserAuthorized} setAdminAuthorized={setAdminAuthorized} />
+  </Route>
+  <Route exact path="/signup">
+    <Signup />
+  </Route>
+</Switch> */}
